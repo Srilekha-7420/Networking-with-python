2ad83bdc50d0706fe838e17af4d0d3031e645b0b
Srilekh@7 <tsrilekha7@gmail.com>
2022-12-29 16:39:57 +0530

    first commit

---
+++ 1.py
@@ -0,0 +1,30 @@
+#print addition of multiple numbers
+a=10
+b=20
+c=a+b
+print(c)
+# print length of string
+a="hclemp"
+c=0
+for i in a:
+    c=c+1
+print(c)
+
+#print "python is amazing" use tab space and newline character
+print("python is \namazing    ")
+
+#read two strings in two different variables,concat them and print length of concated string
+s1='hi'
+s2='hlo'
+s3=s1+s2
+print(len(s3))
+#print remainder,quotient of the division.print both integer and float results
+#print typeof() various kinds of variables
+a=20
+b=6
+print(a/b)
+print(a//b)
+print(type(12.4))
+print(type(a))
+c='hi'
+print(type(c))
